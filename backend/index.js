8057fb9182875cb346d765f08864eb7c854f602d
Taslim <taslim.edu@gmail.com>
2025-09-21 21:25:49 +0700

    Initial commit – backend + frontend

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+      maxAge: 30 * 24 * 60 * 60 * 1000,
       secure: false, // true if HTTPS in production
     },
   })
